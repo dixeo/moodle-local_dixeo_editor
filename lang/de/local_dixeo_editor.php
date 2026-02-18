bc6768710d9bb0ac2a423e69058d7a3b0cde65b4
Josemaria Bolanos <josemabol@gmail.com>
2026-02-17 19:58:11 -0500

    Add portuguese and german translations

---
+++ lang/de/local_dixeo_editor.php
@@ -0,0 +1,44 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ *
+ * @package    local_dixeo_editor
+ * @copyright  2025 Edunao SAS (contact@edunao.com)
+ * @author     Pierre FACQ <pierre.facq@edunao.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Dixeo-KI-Editor';
+$string['dixeo_editor:editpage'] = 'Ermöglicht Nutzer/innen den Zugriff auf KI-Funktionen zur Bearbeitung einer Seitenaktivität';
+$string['prompt_placeholder'] = 'Bearbeitungsanweisungen für die KI';
+$string['generate'] = 'Generieren';
+$string['loading'] = 'Neuer Inhalt wird generiert, bitte warten...';
+$string['apply'] = 'OK';
+$string['content_modification_success'] = 'Inhalt erfolgreich geändert!';
+$string['editcontent'] = 'Dixeo-KI-Editor';
+$string['cancel'] = 'Bearbeitungsmodus beenden';
+$string['save'] = 'Speichern';
+$string['translate'] = 'Übersetzen';
+$string['enrich'] = 'Anreichern';
+$string['translate_prompt'] = 'Inhalt in SPRACHE übersetzen';
+$string['enrich_prompt'] = 'Inhalt zu Bildungszwecken anreichern durch detaillierte Erklärungen, relevante Beispiele und Kontext, um das Material umfassender und ansprechender zu gestalten. Klarheit, Genauigkeit und einen zugänglichen Ton für ein diverses Publikum sicherstellen';
+$string['prettify'] = 'Verschönern';
+$string['prettify_prompt'] = 'Inhalt zu Bildungszwecken verbessern, indem er mit modernen Designprinzipien visuell ansprechender gestaltet wird. Passende Farben, Fett-, Kursiv- und andere Formatierungen hinzufügen, um Schlüsselbegriffe, Überschriften und Ideen hervorzuheben. Klare und ansprechende Ästhetik ohne Änderung des ursprünglichen Inhalts';
---
+++ lang/pt/local_dixeo_editor.php
@@ -0,0 +1,44 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ *
+ * @package    local_dixeo_editor
+ * @copyright  2025 Edunao SAS (contact@edunao.com)
+ * @author     Pierre FACQ <pierre.facq@edunao.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Editor Dixeo AI';
+$string['dixeo_editor:editpage'] = 'Permite ao utilizador aceder às funcionalidades de IA para editar uma atividade de página';
+$string['prompt_placeholder'] = 'Instruções de edição para a IA';
+$string['generate'] = 'Gerar';
+$string['loading'] = 'A gerar novo conteúdo, aguarde...';
+$string['apply'] = 'OK';
+$string['content_modification_success'] = 'Conteúdo modificado com sucesso!';
+$string['editcontent'] = 'Editor Dixeo AI';
+$string['cancel'] = 'Sair do modo de edição';
+$string['save'] = 'Guardar';
+$string['translate'] = 'Traduzir';
+$string['enrich'] = 'Enriquecer';
+$string['translate_prompt'] = 'Traduzir o conteúdo para IDIOMA';
+$string['enrich_prompt'] = 'Melhorar o conteúdo para fins educativos, adicionando explicações detalhadas, exemplos relevantes e contexto para tornar o material mais completo e envolvente para os formandos. Garantir clareza, precisão e um tom acessível adequado a um público diversificado';
+$string['prettify'] = 'Embelezar';
+$string['prettify_prompt'] = 'Melhorar o conteúdo para fins educativos, tornando-o mais atraente visualmente usando princípios de design modernos. Adicionar cores apropriadas, negrito, itálico e outros estilos de formatação para enfatizar termos-chave, títulos e ideias. Usar uma estética limpa e envolvente sem alterar o conteúdo original';
